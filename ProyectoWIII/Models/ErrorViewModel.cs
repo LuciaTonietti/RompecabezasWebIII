3122f816c551e17e2fe2ec067c4ed97129561fe6
luciatonietti <toniettilucia@gmail.com>
2023-05-25 20:18:09 -0300

    suba proyecto base

--- ProyectoWIII/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ProyectoWIII.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
