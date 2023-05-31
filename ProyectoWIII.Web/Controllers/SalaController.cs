ffe88c15a7cb4a84b469df2425b3101c4b670c7f
rodrigo <rodrigo.matto@ayi.group>
2023-05-31 10:02:53 -0300

    Creacion SalaController

---
+++ ProyectoWIII.Web/Controllers/SalaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Rompecabezas.Web.Controllers
+{
+    public class SalaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
