556d0bd6d6edfdd3cf815bccbc31553e10f8bc6e
rodrigo <rodrigo.matto@ayi.group>
2023-06-02 19:30:45 -0300

    Crear sala

--- ProyectoWIII.Web/Controllers/SalaController.cs
+++ ProyectoWIII.Web/Controllers/SalaController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Rompecabezas.Logica.Entidades;
 
 namespace Rompecabezas.Web.Controllers
 {
@@ -6,6 +7,22 @@ namespace Rompecabezas.Web.Controllers
     {
         public IActionResult Crear()
         {
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult Crear(IFormCollection form)
+        {
+            if (ModelState.IsValid)
+            {
+                Sala sala = new Sala();
+                sala.NombreUsuarioCreador = form["NombreUsuarioCreador"];
+                sala.CantPiezas = int.Parse(form["CantPiezas"]);
+                string Pin = form["Pin"];
+                sala.Pin = string.IsNullOrEmpty(Pin) ? null : Pin;
+                TempData["nroSala"] = 1;//Este valor lo deberia devolver el servicio cuando crea la sala en la base de datos.
+                return RedirectToAction("Ingresar");
+            }
             return View();
         }
     }
