62c4ff555314af7b856388cd0081822ce7078ca8
Aibar Hernan Ezequiel <aibarhernanezequiel@gmail.com>
2023-06-03 11:53:47 -0500

    agrega capa de servicios con entityFrameWork

---
+++ ProyectoWIII.Logica/Servicios/EntityFrameworkService.cs
@@ -0,0 +1,32 @@
+﻿using Rompecabezas.Logica.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rompecabezas.Logica.Servicios
+{
+    public class EntityFrameworkService : IEntityFrameworkService
+    {
+        private readonly RompeCabezaPw3Context _context;
+
+        public EntityFrameworkService(RompeCabezaPw3Context context )
+        {
+            _context = context;
+        }
+
+        public void AddSala(Sala sala)
+        {
+            _context.Salas.Add( sala );
+            _context.SaveChanges();
+        }
+
+        public Sala? GetSalaById(int id) => _context.Salas.Where(u => u.Id == id).First();
+
+        public Sala GetSalaByNumber(int nroSala) => _context.Salas.Where(u => u.NroSala == nroSala).First();
+
+        public List<Sala> GetAllSala() => _context.Salas.ToList();  
+ 
+    }
+}
--- ProyectoWIII.Web/Program.cs
+++ ProyectoWIII.Web/Program.cs
@@ -1,4 +1,6 @@
-using Rompecabezas.Logica.Servicios.Base;
+using Microsoft.EntityFrameworkCore;
+using Rompecabezas.Logica.Models;
+using Rompecabezas.Logica.Servicios;
 
 namespace Rompecabezas.Web
 {
@@ -10,7 +12,8 @@ namespace Rompecabezas.Web
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
-            builder.Services.AddSingleton<IAddSingleton, AddSingleton>();
+            builder.Services.AddSingleton<IEntityFrameworkService,EntityFrameworkService>();
+            builder.Services.AddDbContext<RompeCabezaPw3Context>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("RompeCabezaPW3")));
 
             var app = builder.Build();
 
