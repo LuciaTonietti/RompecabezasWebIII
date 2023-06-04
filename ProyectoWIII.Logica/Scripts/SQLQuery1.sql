60c36c3fae25b13b8443562a1d2af7f6cba895f7
Aibar Hernan Ezequiel <aibarhernanezequiel@gmail.com>
2023-06-03 20:53:49 -0500

    agrega script y comando

---
+++ ProyectoWIII.Logica/Scripts/SQLQuery1.sql
@@ -0,0 +1,20 @@
+use RompeCabezaPW3;
+
+go
+CREATE TABLE ScoreMap(
+	id INT IDENTITY(1,1) PRIMARY KEY,
+	nickName VARCHAR(256) unique,
+	score float
+);
+go
+
+go
+CREATE TABLE Sala(
+	id INT IDENTITY(1,1) PRIMARY KEY,
+	nickName VARCHAR(256) unique,
+	cant_pieces INT,
+	pin VARCHAR(4),
+	score_map INT,
+	FOREIGN KEY (score_map) REFERENCES ScoreMap(id)
+);
+go
