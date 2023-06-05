0e1ae56a13d02dbbdf63c7ae005cdb8c2b819e6a
rodrigo <rodrigo.matto@ayi.group>
2023-06-04 23:03:32 -0300

    Agrega opcion 25 piezas en crear sala.

---
+++ ProyectoWIII.Web/Models/Sala.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Rompecabezas.Web.Models;
+
+public partial class Sala
+{
+    public int Id { get; set; }
+
+    public string? NickName { get; set; }
+
+    public int? CantPieces { get; set; }
+
+    public string? Pin { get; set; }
+
+    public int? ScoreMap { get; set; }
+
+    public int? NroSala { get; set; }
+
+    public virtual ScoreMap? ScoreMapNavigation { get; set; }
+}
---
+++ ProyectoWIII.Web/Models/ScoreMap.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Rompecabezas.Web.Models;
+
+public partial class ScoreMap
+{
+    public int Id { get; set; }
+
+    public string? NickName { get; set; }
+
+    public double? Score { get; set; }
+
+    public virtual ICollection<Sala> Salas { get; set; } = new List<Sala>();
+}
