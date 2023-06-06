9786089d986898df7fa626ad8cca27a3bb57a95f
sicilianoaldana <aldanasiciliano@gmail.com>
2023-06-06 16:34:18 -0300

    agregue validaciones y termine estilos

--- ProyectoWIII.Logica/Models/Sala.cs
+++ ProyectoWIII.Logica/Models/Sala.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Rompecabezas.Logica.Models;
 
@@ -7,14 +8,16 @@ public partial class Sala
 {
     public int Id { get; set; }
 
+    [Required(ErrorMessage = "Por favor Ingrese un nombre de usuario.")]
     public string? NickName { get; set; }
-
+    [Required(ErrorMessage = "Por favor seleccione la cantidad de piezas.")]
     public int? CantPieces { get; set; }
-
+    [Required(ErrorMessage = "El limite de participantes es requerido!.")]
     public int? LimiteParticipantes { get; set; }
 
+    [StringLength(4, ErrorMessage = "El PIN debe tener exactamente 4 caracteres.")]
+    [RegularExpression(@"^[0-9]{4}$", ErrorMessage = "El PIN debe ser un numero de 4 caracteres.")]
     public string? Pin { get; set; }
-
     public int? NroSala { get; set; }
 
     public virtual ICollection<ScoreMap> ScoreMaps { get; set; } = new List<ScoreMap>();
