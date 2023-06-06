6fd9a97c501bafaa4e747bfc4c88f8859ff0e315
sicilianoaldana <aldanasiciliano@gmail.com>
2023-06-06 11:17:51 -0300

    actualizo rama

--- ProyectoWIII.Logica/Models/RompeCabezaPw3Context.cs
+++ ProyectoWIII.Logica/Models/RompeCabezaPw3Context.cs
@@ -21,7 +21,7 @@ public partial class RompeCabezaPw3Context : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=WIN10-2; Database=RompeCabezaPW3; TrustServerCertificate=True; User Id=sa; Password=Bebenegro04;");
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-2VFKTQS\\SQLEXPRESS02; Database=RompeCabezaPW3; TrustServerCertificate=True; User Id=sa; Password=password;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
